0e27229af848bbba3449fea238cb817bb12ec9b1
Slawomir Grzebyk <grzebyk.slawomir@gmail.com>
2024-03-25 14:36:47 +0000

    change styling

--- src/app/project_1/page.jsx
+++ src/app/project_1/page.jsx
@@ -1,17 +1,17 @@
-import styles from "../page.module.css";
-
+// import styles from "../page.module.css";
 
 export default function Project1() {
-    return <main className={styles.main}>
-
-        <h1>Project1</h1>
-
-        <div style={{maxWidth:'65rem' }}>
-            <iframe src="p1.html" width="100%" height="1500px" style={{border: "none"}}/>
-
-        </div>
-
-
+  return (
+    <main>
+      <h1>Project1</h1>
+      <div style={{ maxWidth: "65rem" }}>
+        <iframe
+          src="p1.html"
+          width="100%"
+          height="1500px"
+          style={{ border: "none" }}
+        />
+      </div>
     </main>
+  );
 }
-
--- src/components/Manu.tsx
+++ src/components/Manu.tsx
@@ -1,64 +1,71 @@
-'use client'
+"use client";
 
-import React, {useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
 
-
-import { Menu } from 'antd';
+import { Menu } from "antd";
 
 const Manu: React.FC = () => {
-    const [current, setCurrent] = useState('about');
-    const router = useRouter();
+  const [current, setCurrent] = useState("about");
+  const router = useRouter();
 
-
-    const items: MenuProps['items'] = [
+  const items: MenuProps["items"] = [
+    {
+      key: "about",
+      icon: <MailOutlined />,
+      label: "About me",
+    },
+    {
+      label: "TO_DO_LATER",
+      key: "app",
+      icon: <AppstoreOutlined />,
+      disabled: true,
+    },
+    {
+      label: "Projects",
+      key: "projects",
+      icon: <SettingOutlined />,
+      children: [
         {
-            key: 'about',
-            icon: <MailOutlined />,
-            label: 'About me'
+          label: "Project 1",
+          key: "project_1",
         },
         {
-            label: 'TO_DO_LATER',
-            key: 'app',
-            icon: <AppstoreOutlined />,
-            disabled: true,
+          label: "Project 2",
+          key: "project_2",
         },
-        {
-            label: 'Projects',
-            key: 'projects',
-            icon: <SettingOutlined />,
-            children: [
-                {
-                    label: 'Project 1',
-                    key: 'project_1',
-                },
-                {
-                    label: 'Project 2',
-                    key: 'project_2',
-                },
-            ],
-        },
-    ];
+      ],
+    },
+  ];
 
-    const onClick: MenuProps['onClick'] = (e) => {
-        setCurrent(e.key);
-        if (e.key === 'about' ) {
-            router.push('/about');
-        }
-        if (e.key === 'project_1' ) {
-            router.push('/project_1');
-        }
-        if (e.key === 'project_2' ) {
-            router.push('/project_2');
-        }
-    };
+  const onClick: MenuProps["onClick"] = (e) => {
+    setCurrent(e.key);
+    if (e.key === "about") {
+      router.push("/about");
+    }
+    if (e.key === "project_1") {
+      router.push("/project_1");
+    }
+    if (e.key === "project_2") {
+      router.push("/project_2");
+    }
+  };
 
-    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items}
-                 style ={{padding: '1rem 1rem 0.3rem 1rem'}}
-
-    />;
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+      style={{ padding: "1rem 1rem 0.3rem 5rem" }}
+    />
+  );
 };
 
 export default Manu;
