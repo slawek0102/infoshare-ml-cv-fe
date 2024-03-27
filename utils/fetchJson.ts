fc44be96d6c2d504662ceed45fd9c00261ec891b
Slawomir Grzebyk <grzebyk.slawomir@gmail.com>
2024-03-27 15:38:17 +0000

    change href

--- utils/fetchJson.ts
+++ utils/fetchJson.ts
@@ -1,12 +1,12 @@
 export async function postData(url = "", data = {}) {
   const requestOptions = {
-    method: "GET",
+    // method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   };
 
   try {
-    const response = await fetch(url);
+    const response = await fetch("https://restcountries.com/v3.1/all");
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
